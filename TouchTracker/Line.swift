e7c8855f3fb8ff99dfd0ac71d477d46572e2ef18
AutoAvaliar <cs.couto@outlook.com>
2017-06-28 12:05:22 -0300

    page 673

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by AutoAvaliar on 28/06/17.
+//  Copyright © 2017 CoutoCode. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
